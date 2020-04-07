50a79e52f5557db8957ca4abe2968c178872ac0c
yumartins <yuri@estudioflow.com.br>
2020-04-06 22:25:28 -0300

    refactor: more icons and apply routes

--- web/src/layout/Sidebar/index.js
+++ web/src/layout/Sidebar/index.js
@@ -1,7 +1,25 @@
 import React from 'react';
-import { Article, Image } from './styles';
+import { Article, Image, Navigation, Item } from './styles';
 import Logo from '../../assets/logo.svg';
-import { Icon, activity } from '../../components/Icon';
+import { Icon, book, layout, messageCircle } from '../../components/Icon';
+
+const routes = [
+  {
+    icon: layout,
+    route: '/',
+    label: 'Dashboard',
+  },
+  {
+    icon: book,
+    route: 'books,',
+    label: 'Agendar consultas',
+  },
+  {
+    icon: messageCircle,
+    route: '/messages',
+    label: 'Conversas',
+  },
+];
 
 const Sidebar = () => (
   <Article>
@@ -10,7 +28,17 @@ const Sidebar = () => (
       alt="Neo Medic"
     />
 
-    <Icon name={activity}/>
+    <Navigation>
+      {routes.map((item) => (
+        <Item
+          to={item.route}
+          key={item.label}
+        >
+          <Icon name={item.icon} />
+          {item.label}
+        </Item>
+      ))}
+    </Navigation>
   </Article>
 );
 
--- web/src/layout/Sidebar/styles.js
+++ web/src/layout/Sidebar/styles.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import { color } from '../../styles';
+import { Link } from 'react-router-dom';
 
 export const Image = styled.img`
   height: 32px;
@@ -13,3 +14,7 @@ export const Article = styled.article`
   height: 100vh;
   box-shadow: 32px 0 24px 0 rgba(217, 228, 255, .24);
 `;
+
+export const Navigation = styled.div``;
+
+export const Item = styled(Link)``;
