469552b9863c4574869975773b2740cdfecfadeb
yumartins <yuri@estudioflow.com.br>
2020-04-08 22:13:38 -0300

    feat: init mapbox

--- web/src/views/Dash/index.js
+++ web/src/views/Dash/index.js
@@ -1,6 +1,40 @@
-import React from 'react';
-import { typograph } from '../../styles';
+import React, { useState, useEffect } from 'react';
+import MapGL, { GeolocateControl } from 'react-map-gl';
+import "mapbox-gl/dist/mapbox-gl.css";
 
-const Dashboard = () => <h1>Dashboard</h1>;
+const TOKEN = 'pk.eyJ1IjoieXVtYXJ0aW5zIiwiYSI6ImNqam45cXIwMDFtZzMza3M0bnlvMzVrcWsifQ.KHCF7HgUdrQKWMB0hTtq8A';
+
+const geolocateStyle = {
+  float: 'left',
+  margin: '50px',
+  padding: '10px'
+};
+
+const Dashboard = () => {
+  const [viewport, setViewPort] = useState({
+    width: "100%",
+    height: 600,
+    latitude: 0,
+    longitude: 0,
+    zoom: 2
+  });
+
+  const onViewportChange = (viewport) => setViewPort({...viewport, transitionDuration: 3000 });
+
+  return (
+    <MapGL
+      {...viewport}
+      mapboxApiAccessToken={TOKEN}
+      mapStyle="mapbox://styles/yumartins/cjy3h75rr2okn1cr9ug0qwkfi"
+      onViewportChange={onViewportChange}
+    >
+      <GeolocateControl
+        style={geolocateStyle}
+        positionOptions={{enableHighAccuracy: true}}
+        trackUserLocation={true}
+      />
+    </MapGL>
+  );
+};
 
 export default Dashboard;
