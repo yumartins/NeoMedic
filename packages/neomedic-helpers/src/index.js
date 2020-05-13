fffb00f9f828be547793352fb125ebc473119470
yumartins <yuri@estudioflow.com.br>
2020-05-13 20:35:54 -0300

    feat: hook memoize states

---
+++ packages/neomedic-helpers/src/index.js
@@ -0,0 +1,42 @@
+import {
+  useMemo,
+  useReducer as _useReducer,
+} from 'react';
+
+/**
+ * Hook to memoize store state.
+ */
+export const useStoreModule = (state, actions, extra = {}) => useMemo(() => ({
+  ...state,
+  ...extra,
+
+  /**
+   * Call an action.
+   */
+  run (key, ...payload) {
+    const action = actions[key];
+
+    if (! action) {
+      throw new Error(`Action ${key} not defined`);
+    }
+
+    return action(...payload);
+  },
+}), [actions, extra, state]);
+
+
+/**
+ * Abstraction on top of use reducer.
+ */
+export const useReducer = (reducer, initial, init) => {
+  const [state, _dispatch] = _useReducer(reducer, initial, init);
+
+  const dispatch = (type, payload) => {
+    _dispatch({
+      type,
+      payload,
+    });
+  };
+
+  return [state, dispatch];
+};
