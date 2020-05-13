e7978cf420660fb2a3bdc2b7419d0add13cad1c6
yumartins <yuri@estudioflow.com.br>
2020-05-13 20:29:46 -0300

    feat: axios

---
+++ packages/neomedic-authorization/src/api.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: process.env.BASE_API,
+  'Content-Type': 'application/json',
+});
+
+export default api;
---
+++ packages/neomedic-authorization/src/index.js
@@ -0,0 +1,7 @@
+export {
+  default as api,
+} from './api';
+
+export {
+  default as auth,
+} from './auth';
